72e763c2b34fd300fb59fcbd04feb9f53b32cc0d
Martin Homuth <martin@martinhomuth.de>
2017-06-17 13:28:48 +0200

    more on collections
    Signed-off-by: Martin Homuth <martin@martinhomuth.de>

--- 11-collections.lisp
+++ 11-collections.lisp
@@ -65,3 +65,68 @@
 (defparameter *v* #((a 10) (b 20) (a 30) (b 40)))
 (defun verbose-first (x) (format t "Looking at ~s~%" x) (first x))
 (count 'a *v* :key 'verbose-first)
+
+(count-if 'zerop #(0 1 2 3 4))
+(remove-if 'numberp #(0 1 2 3 4 "hans"))
+(position-if-not 'digit-char-p #(#\w #\b #\2 #\,))
+
+(count-if 'evenp #((a 10) (b 25) (c 10) (d 43)) :key 'second)
+(remove-if-not 'alpha-char-p #("foo" "bar" "1baz") :key #'(lambda (x) (elt x 0)))
+
+(defparameter *someseq* #(1 2 3 4 5))
+(defparameter *someotherseq* (copy-seq *someseq*))
+(equalp *someseq* *someotherseq*)
+(eql *someseq* *someotherseq*)
+
+(reverse *someseq*)
+
+(concatenate 'list #(1 2 3) #(3 4 5))
+(concatenate 'string "abs" "olut" "egarbage")
+
+(sort (vector "foo" "bar" "baz") 'string<)
+
+(defparameter *somevec* (vector "foo" "bar" "baz"))
+(sort *somevec* 'string>)
+*somevec* ; modified
+
+;;; better:
+(defparameter *somevec* (vector "foo" "bar" "baz"))
+(sort (copy-seq *somevec*) 'string>)
+
+(merge 'string "asdj" "ertn" 'string<)
+
+(subseq "thisissomething" 4)
+
+(defparameter *x* "somethingsfishy")
+
+(setf (subseq *x* 3 7) "xxxx")
+*x*
+
+(fill *x* #\w :start 7)
+
+(mismatch "foobarbaz" "foom")
+(mismatch #((a 10) (b 20) (c 30)) #((a 10) (b 30) (c 60)) :key 'second)
+
+(every 'evenp #(1 2 3 4 5))
+(every 'evenp #(2 2 4 6))
+
+(some 'evenp #(1 2 3 4 5))
+
+(notany 'evenp #(1 2 3 4 5))
+(notany 'evenp #(1 3 5))
+
+(notevery 'evenp #(2 4))
+
+(every '< #(1 2 3 4) #(5 6 7 8))
+
+(map 'vector '* #(1 2 3 4) #(5 6 7 8))
+
+(defparameter a (vector 1 2 3))
+(defparameter b (vector 4 3 2))
+(defparameter c (vector 9 3 2))
+
+(map-into a '+ a b c)
+
+(reduce '+ #( 1 2 3 4 5 6 7 8))
+
+
